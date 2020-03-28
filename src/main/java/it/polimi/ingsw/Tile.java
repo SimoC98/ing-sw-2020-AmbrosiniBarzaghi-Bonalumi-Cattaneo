a25cf21c4da6b32a720af3b006c49f2cadb1e823
simone <simone6.cattaneo@mail.polimi.it>
2020-03-28 18:40:54 +0100

    test commit

--- src/main/java/it/polimi/ingsw/Tile.java
+++ src/main/java/it/polimi/ingsw/Tile.java
@@ -1,5 +1,22 @@
 package it.polimi.ingsw;
 
 public class Tile {
+    private final int x;
+    private final int y;
+    private int level;
+
+    public Tile(int x, int y) {
+        this.x = x;
+        this.y = y;
+        level = 0;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
 
 }

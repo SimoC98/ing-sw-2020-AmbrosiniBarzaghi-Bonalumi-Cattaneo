3b9875b4837b3842a1ef3dc915ceb3eca6f2b71c
MBonalumi <MBonalumi@users.noreply.github.com>
2020-03-29 17:23:19 +0200

    test

--- src/main/java/it/polimi/ingsw/model/Tile.java
+++ src/main/java/it/polimi/ingsw/model/Tile.java
@@ -4,12 +4,26 @@ public class Tile {
     private final int x;
     private final int y;
     private int level;
+    private Boolean isDomed;
+    private Boolean isOccupied;
+    private Worker worker;
 
 
     public Tile(int x, int y) {
         this.x = x;
         this.y = y;
         level = 0;
+        isDomed = false;
+        isOccupied = false;
+        worker = null;
+    }
+
+    public void increaseLevel()
+    {
+        if(level < 3)
+            level++;
+        else if(level == 3)
+            setDomed();
     }
 
     public int getX() {
@@ -20,4 +34,25 @@ public class Tile {
         return this.y;
     }
 
+    public int getLevel() { return level; }
+
+    public Boolean isDomed() { return isDomed; }
+
+    public Boolean isOccupied() { return isOccupied; }
+
+    public Worker getWorker() { return worker; }
+
+    public void setDomed() {
+        isDomed = true;
+    }
+
+    public void occupy() {
+        isOccupied = true;
+    }
+
+    public void free() { isOccupied = false; }
+
+    public void setWorker(Worker worker) {
+        this.worker = worker;
+    }
 }
--- src/main/java/it/polimi/ingsw/model/Worker.java
+++ src/main/java/it/polimi/ingsw/model/Worker.java
@@ -9,13 +9,33 @@ public class Worker {
         //this.player = null;
     }
 
-
     public Worker(Tile initialPosition) {
         this.positionOnBoard = initialPosition;
         //this.player = player;
     }
 
+    public void move(Tile t){
+        t.occupy();
+        t.setWorker(this);
+        positionOnBoard = t;
+    }
+
+//    public void legalMove(Tile t){}
+
+    //public void build (Tile t){}
+
+//    public void legalBuild(Tile t){}
+
     public Tile getPositionOnBoard() {
         return this.positionOnBoard;
     }
+
+    public void setPositionOnBoard(Tile t) {
+        this.positionOnBoard = t;
+    }
+
+//    public void getPlayer(){
+//    public void getPlayer(){
+//        return player;
+//    }
 }

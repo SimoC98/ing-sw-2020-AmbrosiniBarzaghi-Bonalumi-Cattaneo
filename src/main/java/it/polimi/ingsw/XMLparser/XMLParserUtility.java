90902695f33bcdfd58bf495196c8d9015e8e435b
Marco Bonalumi <10607992@polimi.it>
2020-04-23 01:06:51 +0200

    Self-written XMLParser using DOM and creating a Map<String, Divinity>.

---
+++ src/main/java/it/polimi/ingsw/XMLparser/XMLParserUtility.java
@@ -0,0 +1,110 @@
+package it.polimi.ingsw.XMLparser;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import it.polimi.ingsw.model.Divinity;
+import it.polimi.ingsw.model.cards.StandardDivinity;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
+//TODO: use DOM
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import org.xml.sax.*;
+
+public class XMLParserUtility {
+
+    //given a StandardDivinity and the String containing the path to the DivinityDecoratorWithEffects's son class, instantiates a Divinity
+    private static Divinity parseDivinity(StandardDivinity stdDiv, String divClassPath) {
+        /*
+            code suggested in:
+            https://stackoverflow.com/questions/6094575/creating-an-instance-using-the-class-name-and-calling-constructor/6094602
+         */
+        Class<?> c = null;
+        try {
+            c = Class.forName(divClassPath);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Constructor<?> cons = c.getConstructors()[1];
+
+        Object obj = null;
+        try {
+            obj = cons.newInstance(stdDiv);
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        return (Divinity) obj;
+    }
+
+    //opens resources/divinities.xml and parses the divinities
+    private static Map<String, Divinity> XMLParse() {
+        File xmlFile = new File("resources/divinities.xml");
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = null;
+        try {
+            builder = factory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+        Document doc = null;
+        try {
+            doc = builder.parse(xmlFile);
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Element root = doc.getDocumentElement();
+        NodeList divinities = root.getElementsByTagName("divinity");
+        Map<String, Divinity> divinitiesList = new HashMap<>();
+
+        StandardDivinity stdDiv;
+        String name=null, heading=null, description=null, divClassStr;
+        int number = 0;
+        //TODO: guarda questi for di singola riga annidati e vergognati, sistemare!!
+        for(int i=0; i < divinities.getLength(); i++) {
+            int j;
+
+            NodeList divNodeChildren = divinities.item(i).getChildNodes();
+            for(j=0; divNodeChildren.item(j).getNodeType() != Node.ELEMENT_NODE; j++);
+            divClassStr = divNodeChildren.item(j).getTextContent();
+            for(j++; divNodeChildren.item(j).getNodeType() != Node.ELEMENT_NODE; j++);
+            NodeList stdDivNodeChildren = divNodeChildren.item(j).getChildNodes();
+
+            for(j=0; stdDivNodeChildren.item(j).getNodeType() != Node.ELEMENT_NODE; j++);
+            number = Integer.parseInt(stdDivNodeChildren.item(j).getTextContent());
+            for(j++; stdDivNodeChildren.item(j).getNodeType() != Node.ELEMENT_NODE; j++);
+            name = stdDivNodeChildren.item(j).getTextContent();
+            for(j++; stdDivNodeChildren.item(j).getNodeType() != Node.ELEMENT_NODE; j++);
+            heading = stdDivNodeChildren.item(j).getTextContent();
+            for(j++; stdDivNodeChildren.item(j).getNodeType() != Node.ELEMENT_NODE; j++);
+            description = stdDivNodeChildren.item(j).getTextContent();
+
+            stdDiv = new StandardDivinity(name, heading, description, number);
+
+            divinitiesList.put(name, parseDivinity(stdDiv, divClassStr));
+        }
+
+        return divinitiesList;
+    }
+
+    //this method is meant for giving a meaningful name to the method user will call, and leave an explicative one to XMLParse()
+    public static Map<String, Divinity> getDivinityMap() {
+        return XMLParse();
+    }
+
+}
---
+++ src/test/java/it/polimi/ingsw/XMLparser/XMLParserUtilityTest.java
@@ -0,0 +1,66 @@
+package it.polimi.ingsw.XMLparser;
+
+import it.polimi.ingsw.model.*;
+import it.polimi.ingsw.model.cards.StandardDivinity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class XMLParserUtilityTest {
+
+    Map<String, Divinity> divinities;
+
+    @BeforeEach
+    void setup() {
+        divinities = XMLParserUtility.getDivinityMap();
+    }
+
+    @Test
+    public void parsingTest(){
+        StandardDivinity stdDiv = (StandardDivinity) divinities.get("Apollo").getDivinity();
+        assertEquals("Apollo", stdDiv.getName());
+        assertEquals(1, stdDiv.getNumber());
+
+        stdDiv = (StandardDivinity) divinities.get("Atlas").getDivinity();
+        assertEquals(4, stdDiv.getNumber());
+
+        stdDiv = (StandardDivinity) divinities.get("Minotaur").getDivinity();
+        assertEquals(8, stdDiv.getNumber());
+    }
+
+    @Test
+    public void apolloMoveTest(){
+        List<String> pList = new ArrayList<>();
+        pList.add("Marco");
+        pList.add("Gino");
+        Match match = new Match(pList);
+        Game game = new Game(match);
+        Board board = match.getBoard();
+
+        Player p = match.getPlayers().get(0);
+        Player p2 = match.getPlayers().get(1);
+        Tile t1 = board.getTile(1, 2);
+        Tile t2 = board.getTile(2, 2);
+
+        //THE JUICE
+        DivinityDecoratorWithEffects div = (DivinityDecoratorWithEffects) divinities.get("Apollo");
+        p.setDivinity(div);
+
+        p.addWorker(t1);
+        Worker w1 = p.getWorkers().get(0);
+        Worker w2 = new Worker(t2, p2);
+
+        assertTrue(div.legalMove(w1, t2));
+
+        p.move(w1, t2);
+
+        assertEquals(w1.getPositionOnBoard(), t2);
+        assertEquals(w2.getPositionOnBoard(), t1);
+    }
+
+}

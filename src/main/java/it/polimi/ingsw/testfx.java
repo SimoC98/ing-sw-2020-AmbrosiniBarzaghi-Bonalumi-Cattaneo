37d5779f18ab3a0e5ff472313ab6c3eed402e7a5
simone <simone6.cattaneo@mail.polimi.it>
2020-05-26 17:40:22 +0200

    grid pane test in testfx class

--- src/main/java/it/polimi/ingsw/testfx.java
+++ src/main/java/it/polimi/ingsw/testfx.java
@@ -3,6 +3,7 @@ package it.polimi.ingsw;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -14,55 +15,34 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+
 /**
  * JavaFX App
  */
 public class testfx extends Application {
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws IOException {
 
 
         stage.setTitle("HelloDevoxx");
-        GridPane root= new GridPane();
+
+        FXMLLoader playerDivinityLoader = new FXMLLoader(getClass().getResource("/prove/Prova.fxml"));
+        //Parent playableDivinitiesPane = playableDivinitiesLoader.load();
+        Parent root = playerDivinityLoader.load();
+        // Scene loginScene = new Scene(loginPane, 750, 500);
+
         Scene scene=new Scene(root, 400, 250,Color.ALICEBLUE);
 
-        Text text1= new Text();
-        Button b = new Button("press me");
-        text1.setX(105);
-        text1.setY(120);
-        text1.setFont(new Font(30));
-        text1.setText("prova row 1");
-
-        Text text2 = new Text();
-        text2.setX(105);
-        text2.setY(120);
-        text2.setFont(new Font(30));
-        text2.setText("prova row 2");
-
-        StackPane stackpane = new StackPane();
-        stackpane.getChildren().add(new Button("ciao"));
 
 
 
-        root.addRow(0,text1);
-        root.addRow(1,text2);
-        //root.addRow(2,b);
-        root.addColumn(0,b);
 
-        root.getChildren().add(stackpane);
 
-        b.setOnMouseClicked((e)-> {
-            System.out.print("clicked");
 
-            int x = (int)e.getSceneX();
-            int y = (int)e.getSceneY();
 
-            System.out.print(x + y);
-
-            text1.setText("presssssssss");
-            stackpane.setVisible(false);
-        });
         //root.getChildren().add(b);
         stage.setScene(scene);
         stage.show();

2a024722499774accf960c692c15bc0fea7984eb
simone <simone6.cattaneo@mail.polimi.it>
2020-06-12 17:18:59 +0200

    EndGameController and EndGame.fxml

--- src/main/java/it/polimi/ingsw/clientView/gui/EndGameController.java
+++ src/main/java/it/polimi/ingsw/clientView/gui/EndGameController.java
@@ -1,4 +1,46 @@
 package it.polimi.ingsw.clientView.gui;
 
+import it.polimi.ingsw.clientView.ClientView;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
+import static java.lang.System.exit;
+
 public class EndGameController {
+
+    private static ClientView clientView;
+
+    @FXML
+    private VBox panel;
+
+    @FXML
+    private Button exit;
+
+    @FXML
+    private Button newGame;
+
+    @FXML
+    public void initialize() {
+        exit.setText("EXIT");
+        exit.getStyleClass().add("blue");
+        exit.getStylesheets().add("/css/btn.css");
+
+        newGame.setText("NEW GAME");
+        newGame.getStyleClass().add("blue");
+        newGame.getStylesheets().add("/css/btn.css");
+
+        exit.setOnMouseClicked((e) -> {
+            exit(0);
+        });
+
+        newGame.setOnMouseClicked((e) -> {
+            clientView.startConnection();
+        });
+    }
+
+
+    public static void setClientView(ClientView clientView) {
+        EndGameController.clientView = clientView;
+    }
 }

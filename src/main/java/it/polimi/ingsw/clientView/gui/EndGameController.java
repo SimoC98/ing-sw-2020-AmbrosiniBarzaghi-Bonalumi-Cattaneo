756ec7f2a287287bfd0efe22a3407c89e58c1009
simone <simone6.cattaneo@mail.polimi.it>
2020-06-12 18:01:18 +0200

    EndGameController and EndGame.fxml

--- src/main/java/it/polimi/ingsw/clientView/gui/EndGameController.java
+++ src/main/java/it/polimi/ingsw/clientView/gui/EndGameController.java
@@ -3,6 +3,8 @@ package it.polimi.ingsw.clientView.gui;
 import it.polimi.ingsw.clientView.ClientView;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.effect.Light;
+import javafx.scene.effect.Lighting;
 import javafx.scene.layout.VBox;
 
 import static java.lang.System.exit;
@@ -22,6 +24,14 @@ public class EndGameController {
 
     @FXML
     public void initialize() {
+        Light.Distant light = new Light.Distant();
+        light.setAzimuth(-135.0);
+
+        Lighting lighting = new Lighting();
+        lighting.setLight(light);
+        lighting.setSurfaceScale(4.0);
+
+
         exit.setText("EXIT");
         exit.getStyleClass().add("blue");
         exit.getStylesheets().add("/css/btn.css");
@@ -34,8 +44,24 @@ public class EndGameController {
             exit(0);
         });
 
+        exit.setOnMouseEntered((e) -> {
+            exit.setEffect(lighting);
+        });
+
+        exit.setOnMouseExited((e) -> {
+            exit.setEffect(null);
+        });
+
         newGame.setOnMouseClicked((e) -> {
             clientView.startConnection();
+        });
+
+        newGame.setOnMouseEntered((e) -> {
+            newGame.setEffect(lighting);
+        });
+
+        newGame.setOnMouseExited((e) -> {
+            newGame.setEffect(null);
         });
     }
 
